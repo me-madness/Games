373980278d54bfe80ca43dc722b2b703d0631de4
me-madness <58656549+me-madness@users.noreply.github.com>
2023-08-19 18:07:23 +0200

    the ship is moving

--- 06.Pygame_free_code_camp/main.py
+++ 06.Pygame_free_code_camp/main.py
@@ -6,17 +6,20 @@ pygame.init()
 # Create the screen     
 screen = pygame.display.set_mode((800, 660))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('001-ufo.png')
+
 # Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
-
-# Title and Icon
-pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('001-ufo.png')
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+    
+    
 # Game Loop
 running = True
 while running:
@@ -27,6 +30,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    # If keystrokeis pressed check whether its right or left
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -0.3
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.3    
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0    
+
+            
+            
+    playerX += playerX_change
     
-    player()
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+        
+                       
+    player(playerX, playerY)
     pygame.display.update()        
