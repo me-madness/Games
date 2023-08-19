aefa199c780e050ec15007e991ea68cf54d9f188
me-madness <58656549+me-madness@users.noreply.github.com>
2023-08-19 16:57:10 +0200

    icon finish

--- 06.Pygame_free_code_camp/main.py
+++ 06.Pygame_free_code_camp/main.py
@@ -6,5 +6,12 @@ pygame.init()
 # Create the screen     
 screen = pygame.display.set_mode((800, 660))
 
-while True:
-    
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('001-ufo.png')
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
