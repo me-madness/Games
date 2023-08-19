1eb69eaed954ce40af4b5a25eda596e2c7125eb7
me-madness <58656549+me-madness@users.noreply.github.com>
2023-08-19 17:18:40 +0200

    add player image

--- 06.Pygame_free_code_camp/main.py
+++ 06.Pygame_free_code_camp/main.py
@@ -6,16 +6,27 @@ pygame.init()
 # Create the screen     
 screen = pygame.display.set_mode((800, 660))
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('001-ufo.png')
 # Game Loop
 running = True
 while running:
+    # RGB - Red, Green, Blue        
+    screen.fill((0, 0, 0))    
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    # RGB - Red, Green, Blue        
-    screen.fill((0, 0, 0))
+    
+    player()
     pygame.display.update()        
