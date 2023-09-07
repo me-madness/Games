523ffb0453bca748b07419443c191dce84a46656
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-08 00:02:30 +0200

    app file

--- 07. Fruits Web Game/app.py
+++ 07. Fruits Web Game/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, redirect, request
 import random
-
 app = Flask(__name__)
 
+rows = 3
+cols = 9
+
+fruits = generate_random_fruits()
+
+score = 0
+game_over = False
 
 @app.route('/')
 def index():
 	return render_template('index.html')
-
+	rows=rows, cols=cols, fruits=fruits,
+	game_over=game_over, score=score
 
 if __name__ == '__main__':
 	app.run()
