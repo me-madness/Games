979a19afed02cd204246e5f37fc3af0369b71b9f
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-07 23:45:47 +0200

    3 save

--- 07. Fruits Web Game/app.py
+++ 07. Fruits Web Game/app.py
@@ -1,13 +1,13 @@
-from flask \
-    import Flask, render_template, request
-import random    
+from flask import Flask, render_template, redirect, request
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
